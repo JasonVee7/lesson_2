3fed8e7577e6adc7b5ad3a652a7e93e97a1b297b
JasonVee7 <125880073+JasonVee7@users.noreply.github.com>
2023-11-20 21:07:12 +1000

    Update and rename 01_opencv_window.py to 01_opencv_hands_1.py

---
+++ 01_opencv_hands_1.py
@@ -0,0 +1,36 @@
+import cv2
+import mediapipe as mp
+
+# Initialize MediaPipe Hands
+hands = mp.solutions.hands.Hands()
+
+# MediaPipe drawing utilities
+mp_drawing = mp.solutions.drawing_utils
+
+# OpenCV setup
+cap = cv2.VideoCapture(0)  # You can specify a different index if you have multiple cameras
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+
+    # Convert the image to RGB for MediaPipe
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    results = hands.process(rgb_frame)
+
+    # If hands are detected, draw landmarks on the frame
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            mp_drawing.draw_landmarks(
+                frame, hand_landmarks, mp.solutions.hands.HAND_CONNECTIONS)
+
+    # Show the frame with detections
+    cv2.imshow('Hand Tracking', frame)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+# Release the VideoCapture and close the OpenCV windows
+cap.release()
+cv2.destroyAllWindows()
