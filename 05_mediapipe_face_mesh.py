d50a3b8ff492925c3853efe853e79d1bc11c495e
JasonVee7 <125880073+JasonVee7@users.noreply.github.com>
2023-11-20 21:36:41 +1000

    Update 05_mediapipe_face_mesh.py

--- 05_mediapipe_face_mesh.py
+++ 05_mediapipe_face_mesh.py
@@ -1,53 +1,67 @@
 import cv2
 import mediapipe as mp
-import numpy as np
-import requests
+import math
+import pyautogui
 
-# URL of the image
-image_url = "https://github.com/JasonVee7/lesson_2/raw/main/640x960.jpg"
+# Initialize MediaPipe FaceMesh
+face_mesh = mp.solutions.face_mesh.FaceMesh(static_image_mode=False, max_num_faces=1, min_detection_confidence=0.5)
 
-# Load the image from the URL
-response = requests.get(image_url)
-image_array = np.frombuffer(response.content, dtype=np.uint8)
-image = cv2.imdecode(image_array, -1)
+# Open default camera (webcam)
+cap = cv2.VideoCapture(0)
 
-# Create a window and display the image
-cv2.namedWindow('Image Window', cv2.WINDOW_NORMAL)
-cv2.resizeWindow('Image Window', 640, 960)
-cv2.imshow('Image Window', image)
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        continue
 
-# Create a FaceMesh instance
-face_mesh = mp.solutions.face_mesh
+    # Convert BGR image to RGB
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-# Initialize the FaceMesh model
-with face_mesh.FaceMesh(
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.5,
-) as face_mesh_model:
-    results = face_mesh_model.process(image)
+    # Detect face landmarks
+    results = face_mesh.process(rgb_frame)
 
     if results.multi_face_landmarks:
         for face_landmarks in results.multi_face_landmarks:
-            # Convert landmarks to pixel coordinates
-            height, width, _ = image.shape
-            landmark_points = []
-            for landmark in face_landmarks.landmark:
-                x = int(landmark.x * width)
-                y = int(landmark.y * height)
-                landmark_points.append((x, y))
+            # Get landmark coordinates for points 12 and 16 (top and bottom lips)
+            top_lip = face_landmarks.landmark[12]
+            bottom_lip = face_landmarks.landmark[16]
 
-            # Draw landmarks on the image
-            for x, y in landmark_points:
-                cv2.circle(image, (x, y), 2, (0, 255, 0), -1)
+            # Get landmark coordinates
+            ih, iw, _ = frame.shape
+            top_lip_x, top_lip_y = int(top_lip.x * iw), int(top_lip.y * ih)
+            bottom_lip_x, bottom_lip_y = int(bottom_lip.x * iw), int(bottom_lip.y * ih)
 
-# Display the image with landmarks
-cv2.imshow('Image with Face Landmarks', image)
+            # Draw circles around the points (landmark 12 and 16)
+            cv2.circle(frame, (top_lip_x, top_lip_y), 5, (0, 255, 0), -1)
+            cv2.circle(frame, (bottom_lip_x, bottom_lip_y), 5, (0, 255, 0), -1)
 
-# Wait for the window to be closed
-while True:
-    key = cv2.waitKey(1)
-    if key == 27:  # 27 is the ASCII code for the 'Esc' key
+            # Draw a line connecting the points
+            cv2.line(frame, (top_lip_x, top_lip_y), (bottom_lip_x, bottom_lip_y), (0, 255, 0), 2)
+
+            # Calculate Euclidean distance between points 12 and 16
+            distance = math.sqrt((bottom_lip_x - top_lip_x) ** 2 + (bottom_lip_y - top_lip_y) ** 2)
+            distance = round(distance, 2)  # Round the distance to 2 decimal places
+
+             # set threshold
+            if distance > 23:
+                print('mouth open')
+                pyautogui.press('space')
+
+            # Display the distance on the webcam window
+            cv2.putText(frame, f"Distance: {distance}", (20, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
+           
+
+
+    # Display the webcam window with landmarks and distance
+    cv2.imshow('Lips Landmark Detection', frame)
+
+    # Exit the loop by pressing 'q'
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Close the windows
+# Release the VideoCapture and close all windows
+cap.release()
 cv2.destroyAllWindows()
+
+
