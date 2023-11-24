90b4335b456908b7aa37c29632add55ecb696724
JasonVee7 <125880073+JasonVee7@users.noreply.github.com>
2023-11-24 20:58:49 +1000

    Update 04_1_mediapipe_hands_2.py

--- 04_1_mediapipe_hands_2.py
+++ 04_1_mediapipe_hands_2.py
@@ -10,7 +10,6 @@ mp_drawing = mp.solutions.drawing_utils
 # OpenCV setup
 cap = cv2.VideoCapture(0)  # You can specify a different index if you have multiple cameras
 
-
 while cap.isOpened():
     ret, frame = cap.read()
     if not ret:
@@ -28,10 +27,18 @@ while cap.isOpened():
                     x, y = int(landmark.x * frame.shape[1]), int(landmark.y * frame.shape[0])
                     cv2.circle(frame, (x, y), 5, (0, 255, 0), -1)  # Draw a green circle at landmark 9
                     print('9: ', y)
+
+                    # Display the y coordinate of landmark 9 on the frame window
+                    cv2.putText(frame, f"y: {y}", (x + 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+
                 elif idx == 12:
                     x, y = int(landmark.x * frame.shape[1]), int(landmark.y * frame.shape[0])
                     cv2.circle(frame, (x, y), 5, (255, 0, 0), -1)  # Draw a blue circle at landmark 12
                    # print('12: ', y)
+
+                   # Display the y coordinate of landmark 9 on the frame window
+                    cv2.putText(frame, f"y: {y}", (x + 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+
 
     # Show the frame with detections
     cv2.imshow('Hand Tracking', frame)
@@ -42,5 +49,7 @@ while cap.isOpened():
 # Release the VideoCapture and close the OpenCV windows
 cap.release()
 cv2.destroyAllWindows()
+
+
 
 
