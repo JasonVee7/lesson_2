b25da8fd5f0c43d8d52a4fe1b424e59dda4a3192
JasonVee7 <125880073+JasonVee7@users.noreply.github.com>
2023-11-26 14:02:18 +1000

    Update 03_opencv_hands_1.py

--- 03_opencv_hands_1.py
+++ 03_opencv_hands_1.py
@@ -3,12 +3,13 @@ import mediapipe as mp
 
 # Initialize MediaPipe Hands
 hands = mp.solutions.hands.Hands()
-
 # MediaPipe drawing utilities
 mp_drawing = mp.solutions.drawing_utils
 
 # OpenCV setup
 cap = cv2.VideoCapture(0)  # You can specify a different index if you have multiple cameras
+
+# Loop
 while cap.isOpened():
     ret, frame = cap.read()
     if not ret:
@@ -31,6 +32,6 @@ while cap.isOpened():
     if cv2.waitKey(1) == ord('q'):
         break
 
-# Release the VideoCapture and close the OpenCV windows
+# Close
 cap.release()
 cv2.destroyAllWindows()
